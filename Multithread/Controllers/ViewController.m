01a633961e4a87976a0fb9dcd2b05257e912f264
Brunno dos Santos <squiter85@gmail.com>
2015-10-26 11:41:08 -0200

    Get a big image from internet and showing into UIImageView

--- Multithread/Controllers/ViewController.m
+++ Multithread/Controllers/ViewController.m
@@ -30,6 +30,10 @@
 
 - (IBAction)loadImage:(id)sender {
     NSLog(@"Loading...");
+    NSURL *boobUrl = [NSURL URLWithString:@"http://hellogiggles.hellogiggles.netdna-cdn.com/wp-content/uploads/2014/04/14/2013-Kate-Upton-HD-Wallpapers-e1360405079523-500x375c.jpeg"];
+    NSURL *nasaUrl = [NSURL URLWithString:@"http://go.nasa.gov/1NvITOM"];
+    NSData *imageSource = [NSData dataWithContentsOfURL:nasaUrl];
+    self.myImage.image = [UIImage imageWithData:imageSource];
 }
 
 @end
